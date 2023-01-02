8abb712dd966942ab9c589689ac4d27cc695b476
muhaiminul-islam-bjit <muhaimiul.islam@bjitgroup.com>
2023-01-02 16:48:44 +0600

    complete calculations

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,11 +1,28 @@
+import { useSelector } from "react-redux";
+import { numberWithCommas } from "../utils/thousandSeparator";
+
 export default function Balance() {
-    return (
-        <div className="top_card">
-            <p>Your Current Balance</p>
-            <h3>
-                <span>৳</span>
-                <span>10500</span>
-            </h3>
-        </div>
-    );
+  const { transactions } = useSelector((state) => state.transaction);
+  const calculation = () => {
+    let income = 0;
+    transactions.forEach(transaction => {
+        const {type , amount} = transaction;
+        if(type === "income") {
+            income += amount;
+        }else {
+            income -= amount;
+        }
+    });
+
+    return numberWithCommas(income);
+  }
+  return (
+    <div className="top_card">
+      <p>Your Current Balance</p>
+      <h3>
+        <span>৳</span>
+        <span>{transactions.length > 0 ? calculation() : 0}</span>
+      </h3>
+    </div>
+  );
 }
